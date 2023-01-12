b985cf54861956fc4f1a0d7be013625d63b78916
Ogg-Vorbis <Ogg-Vorbis@users.noreply.github.com>
2023-01-12 11:12:50 -0800

    Added color tests and color object. Could try to combine this with the element object in some way since they act pertty much the same, but I'm too lazy.

---
+++ src/RayTracerChallenge.Features/DataStructures/Color.cs
@@ -0,0 +1,36 @@
+﻿namespace RayTracerChallenge.Features.DataStructures;
+
+public readonly struct Color
+{
+    public Color(float red, float green, float blue)
+    {
+        Red=red;
+        Green=green;
+        Blue=blue;
+    }
+
+    public float Red { get; }
+    public float Green { get; }
+    public float Blue { get; }
+
+    public static Color operator +(Color left, Color right)
+    {
+        return new Color(left.Red + right.Red, left.Green + right.Green, left.Blue + right.Blue);
+    }
+
+    public static Color operator -(Color left, Color right)
+    {
+        return new Color(left.Red - right.Red, left.Green - right.Green, left.Blue - right.Blue);
+    }
+
+    public static Color operator *(Color left, Color right)
+    {
+        return new Color(left.Red * right.Red, left.Green * right.Green, left.Blue * right.Blue);
+    }
+
+    public static Color operator *(Color left, float scalar)
+    {
+        return new Color(left.Red * scalar, left.Green * scalar, left.Blue * scalar);
+    }
+
+}
---
+++ test/RayTracerChallenge.Features.Tests/RayTracerChallenge.Features.Tests/DataStructures/ColorTests.cs
@@ -0,0 +1,59 @@
+﻿using RayTracerChallenge.Features.DataStructures;
+
+namespace RayTracerChallenge.Features.Tests.DataStructures;
+
+public class ColorTests
+{
+    [Fact]
+    public void ColorsAreRedGreenBlueTuples()
+    {
+        Color c = new(-0.5f, 0.4f, 1.7f);
+        c.Red.ShouldBeAbout(-0.5f);
+        c.Green.ShouldBeAbout(0.4f);
+        c.Blue.ShouldBeAbout(1.7f);
+    }
+
+    [Fact]
+    public void ColorsAddColors()
+    {
+        Color c1 = new(0.9f, 0.6f, 0.75f);
+        Color c2 = new(0.7f, 0.1f, 0.25f);
+        Color added = c1 + c2;
+        added.Red.ShouldBeAbout(1.6f);
+        added.Green.ShouldBeAbout(0.7f);
+        added.Blue.ShouldBeAbout(1.0f);
+    }
+
+    [Fact]
+    public void ColorsSubtractColors()
+    {
+        Color c1 = new(0.9f, 0.6f, 0.75f);
+        Color c2 = new(0.7f, 0.1f, 0.25f);
+        Color subtracted = c1 - c2;
+        subtracted.Red.ShouldBeAbout(0.2f);
+        subtracted.Green.ShouldBeAbout(0.5f);
+        subtracted.Blue.ShouldBeAbout(0.5f);
+    }
+
+    [Fact]
+    public void ColorsMultiplyByScalar()
+    {
+        Color c = new(0.2f, 0.3f, 0.4f);
+        Color scalar = c * 2;
+        scalar.Red.ShouldBeAbout(0.4f);
+        scalar.Green.ShouldBeAbout(0.6f);
+        scalar.Blue.ShouldBeAbout(0.8f);
+    }
+
+    [Fact]
+    public void ColorsBlendColors()
+    {
+        Color c1 = new(1f, 0.2f, 0.4f);
+        Color c2 = new(0.9f, 1f, 0.1f);
+        Color blended = c1 * c2;
+        blended.Red.ShouldBeAbout(0.9f);
+        blended.Green.ShouldBeAbout(0.2f);
+        blended.Blue.ShouldBeAbout(0.04f);
+    }
+
+}
