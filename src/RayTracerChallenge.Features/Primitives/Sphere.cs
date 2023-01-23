520fd499e22609a6cc8248f888c421dda06d2db2
Ogg-Vorbis <Ogg-Vorbis@users.noreply.github.com>
2023-01-22 20:59:09 -0800

    Added sphere primitive with some basic implementation and some tests.

---
+++ src/RayTracerChallenge.Features/Primitives/Sphere.cs
@@ -0,0 +1,20 @@
+﻿using RayTracerChallenge.Features.DataStructures;
+
+namespace RayTracerChallenge.Features.Primitives;
+
+public class Sphere
+{
+    public float[] Intersect(Ray ray)
+    {
+        var sphereToRay = ray.Origin - Element.CreatePoint(0, 0, 0);
+        var a = Element.Dot(ray.Direction, ray.Direction);
+        var b = 2 * Element.Dot(ray.Direction, sphereToRay);
+        var c = Element.Dot(sphereToRay, sphereToRay) - 1;
+
+        var discriminant = (b * b) - 4 * a * c;
+        if (discriminant < 0) return Array.Empty<float>();
+        var t1 = (-b - MathF.Sqrt(discriminant)) / (2 * a);
+        var t2 = (-b + MathF.Sqrt(discriminant)) / (2 * a);
+        return new float[2] { t1, t2 };
+    }
+}
---
+++ test/RayTracerChallenge.Features.Tests/RayTracerChallenge.Features.Tests/Primitives/SphereTests.cs
@@ -0,0 +1,59 @@
+﻿using RayTracerChallenge.Features.Primitives;
+
+namespace RayTracerChallenge.Features.Tests.Primitives;
+
+public class SphereTests
+{
+    [Fact]
+    public void RayInteresectsSphereAtTwoPoints()
+    {
+        var ray = new Ray(Element.CreatePoint(0, 0, -5f), Element.CreateVector(0, 0, 1));
+        var sphere = new Sphere();
+        float[] xs = sphere.Intersect(ray);
+        xs[0].ShouldBeAbout(4.0f);
+        xs[1].ShouldBeAbout(6.0f);
+    }
+
+    [Fact]
+    public void RayIntersectsSphereAtATangent()
+    {
+        var ray = new Ray(Element.CreatePoint(0, 1, -5f), Element.CreateVector(0, 0, 1));
+        var sphere = new Sphere();
+        float[] xs = sphere.Intersect(ray);
+        xs.Length.ShouldBe(2);
+        xs[0].ShouldBeAbout(5);
+        xs[1].ShouldBeAbout(5);
+    }
+
+    [Fact]
+    public void RayMissesASphereReturnsNoIntersections()
+    {
+        var ray = new Ray(Element.CreatePoint(0, 2, -5f), Element.CreateVector(0, 0, 1));
+        var sphere = new Sphere();
+        float[] xs = sphere.Intersect(ray);
+        xs.Length.ShouldBe(0);
+    }
+
+    [Fact]
+    public void RayOriginatesWithinASphere()
+    {
+        var ray = new Ray(Element.CreatePoint(0, 0, 0), Element.CreateVector(0, 0, 1));
+        var sphere = new Sphere();
+        float[] xs = sphere.Intersect(ray);
+        xs.Length.ShouldBe(2);
+        xs[0] = -1f;
+        xs[1] = 1f;
+    }
+
+    [Fact]
+    public void SphereIsBehindRay()
+    {
+        var ray = new Ray(Element.CreatePoint(0, 0, 5), Element.CreateVector(0, 0, 1));
+        var sphere = new Sphere();
+        float[] xs = sphere.Intersect(ray);
+        xs.Length.ShouldBe(2);
+        xs[0] = -6f;
+        xs[1] = -4f;
+    }
+
+}
