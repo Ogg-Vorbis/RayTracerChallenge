c06fa14cfe778d76834ae9d4af35c1598559529d
Ogg-Vorbis <Ogg-Vorbis@users.noreply.github.com>
2023-01-22 17:19:37 -0800

    Started adding Rays and tests

---
+++ src/RayTracerChallenge.Features/DataStructures/Ray.cs
@@ -0,0 +1,15 @@
+﻿namespace RayTracerChallenge.Features.DataStructures;
+
+public readonly struct Ray
+{
+    public Ray(Element origin, Element direction)
+    {
+        Origin = origin;
+        Direction = direction;
+    }
+
+    public Element Origin { get; }
+    public Element Direction { get; }
+
+    public Element Position(float t) => Origin + Direction * t;
+}
---
+++ test/RayTracerChallenge.Features.Tests/RayTracerChallenge.Features.Tests/DataStructures/RayTests.cs
@@ -0,0 +1,27 @@
+﻿namespace RayTracerChallenge.Features.Tests.DataStructures;
+
+public class RayTests
+{
+    [Fact]
+    public void CreateARay()
+    {
+        var origin = Element.CreatePoint(1, 2, 3);
+        var direction = Element.CreateVector(4, 5, 6);
+        var ray = new Ray(origin, direction);
+        ray.Origin.ShouldBe(origin);
+        ray.Direction.ShouldBe(direction);
+    }
+
+    [Fact]
+    public void ComputeAPointFromADistance()
+    {
+        var r = new Ray(Element.CreatePoint(2, 3, 4), Element.CreateVector(1, 0, 0));
+        r.Position(0f).ShouldBe(Element.CreatePoint(2, 3, 4));
+        r.Position(1f).ShouldBe(Element.CreatePoint(3, 3, 4));
+        r.Position(-1f).ShouldBe(Element.CreatePoint(1, 3, 4));
+        r.Position(2.5f).X.ShouldBeAbout(4.5f);
+        r.Position(2.5f).Y.ShouldBeAbout(3f);
+        r.Position(2.5f).Z.ShouldBeAbout(4);
+    }
+
+}
